a7857a26e192768c855dcf4fa418c83bed8ffbf8
Joshimani <caraiglex@gmail.com>
2026-09-23 15:31:49 -0700

    Implement authentication module with user registration and token management

--- auth/router.py
+++ auth/router.py
@@ -1,2 +1,20 @@
 from fastapi import APIRouter
+from schema import Signup, Login, ForgetPassword, Resetpassword
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from database.database import get_db
+import main
+
+
+router = APIRouter(
+    prefix= "/auth",
+    tags=["auth"]
+
+)
+
+@router.post("/user")
+def user_registeration(request: Signup, db: Session=Depends(get_db)):
+    return main.user_register(request, db)
+
+
 
